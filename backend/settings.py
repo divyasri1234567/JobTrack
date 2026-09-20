b2ad0cfb03fa6716c8d3a773d3d10566760ec157
Divya <divyabantupalli203@gmail.com>
2026-09-20 19:33:04 +0530

    Configure Render PostgreSQL database

--- backend/settings.py
+++ backend/settings.py
@@ -77,17 +77,30 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "jobtrack_db",
-        "USER": "postgres",
-        "PASSWORD": "12345",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "jobtrack_db",
+#         "USER": "postgres",
+#         "PASSWORD": "12345",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
+import os
+
+import dj_database_url
+
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv(
+            "DATABASE_URL",
+            "postgresql://postgres:12345@localhost:5432/jobtrack_db",
+        )
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
